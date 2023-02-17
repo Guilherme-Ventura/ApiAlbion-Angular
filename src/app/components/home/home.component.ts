f5971d1f9749a2a5fb292fecd241f30a78758b2a
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2023-02-17 10:19:49 -0300

    adc interface personagens

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { firstValueFrom } from 'rxjs';
-
 import { Personagem } from 'src/app/interfaces/personagem';
 import { ListService} from 'src/app/services/list.service'
+
 
 @Component({
   selector: 'app-home',
@@ -30,15 +30,20 @@ export class HomeComponent {
   constructor(private listService : ListService) {}
 
   async pesquisarPersonagem(){
-/*     this.listaPersonagens = await firstValueFrom(await this.listService.buscarNomePersonagem(this.nomePersonagem))
- */
+    this.listaPersonagens = await firstValueFrom(await this.listService.buscarNomePersonagem(this.nomePersonagem))
+
     if (this.listaPersonagens.length > 0){
       console.log(this.listaPersonagens);
     }
   }
-  createForm(personagem: Personagem){
+/*   createForm(personagem: Personagem){
     this.formPersonagem = new FormGroup({
       nomePersonagem: new FormControl(personagem.nomePersonagem)
     })
-  } 
+  }  */
+  criarFormulario(personagem: Personagem){
+    this.formPersonagem = new FormGroup({
+      nomePersonagem: new FormControl(personagem.nome)
+    })
+  }
 }
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 
-import { Personagem } from '../personagem';
+import { Personagem } from '../interfaces/personagem';
 
 @Injectable({
   providedIn: 'root'
